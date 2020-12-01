29e983a0d71a0b2fc447a9646307d7430cbebce9
Dmitrii <lexur-02@yandex.ru>
2020-12-01 22:51:41 +0500

    day 1

---
+++ src/day1/easy/index.ts
@@ -0,0 +1,18 @@
+import input from './input';
+
+function main(): number {
+  const year = 2020;
+  const parsedInput = input.split('\n');
+  const memory = parsedInput.reduce<{ [key: string]: true }>((acc, el) => {
+    acc[el] = true;
+    return acc;
+  }, {});
+
+  const firstNumber = Number(parsedInput.find((el) => {
+    return memory[year - Number(el)];
+  }));
+
+  return firstNumber * (2020 - firstNumber);
+}
+
+console.log(`Result of day1/easy -> ${main()}`);
---
+++ src/day1/hard/index.ts
@@ -0,0 +1,29 @@
+import input from './input';
+
+function main(): number {
+  const year = 2020;
+  const parsedInput = input.split('\n');
+  const memory = parsedInput.reduce<{ [key: string]: true }>((acc, el) => {
+    acc[el] = true;
+    return acc;
+  }, {});
+
+  const result = parsedInput.reduce<{ solved: boolean, values: [number, number, number] }>((acc, el) => {
+    if (acc.solved) {
+      return acc;
+    }
+    for (let i = 0; i < parsedInput.length; i += 1) {
+      if (memory[year - Number(el) - Number(parsedInput[i])]) {
+        acc = {
+          solved: true,
+          values: [year - Number(el) - Number(parsedInput[i]), Number(el), Number(parsedInput[i])],
+        };
+      }
+    }
+    return acc;
+  }, { solved: false, values: [0, 0, 0] });
+
+  return result.values[0] * result.values[1] * result.values[2];
+}
+
+console.log(`Result of day1/hard -> ${main()}`);
