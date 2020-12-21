575b1af7aeb7c197a27aeaa9d8aad4ea669a4cc5
Dmitrii <lexur-02@yandex.ru>
2020-12-21 11:46:28 +0500

    day21

---
+++ src/day21/easy/index.ts
@@ -0,0 +1,58 @@
+import input from '../input';
+
+const inputFormatted = input.split('\n').map((el) => {
+  const [values, contains] = el.split('(contains ');
+  return {
+    values: values.split(' ').slice(0, values.split(' ').length - 1),
+    contains: contains.slice(0, contains.length - 1).split(', '),
+  }
+});
+
+const ingredients = inputFormatted.reduce<{[key:string]: true}>((acc, el) => {
+  el.values.forEach(value => {
+    acc[value] = true;
+  });
+  return acc;
+}, {});
+
+const allergens = inputFormatted.reduce<{[key:string]: string | null}>((acc, el) => {
+  el.contains.forEach(value => {
+    acc[value] = null;
+  });
+  return acc;
+}, {});
+
+export function main(): number {
+  const match: {[key: string]: string[]} = {};
+  Object.keys(allergens).forEach((allergen) => {
+    const ingredientsForAllergen = inputFormatted
+      .filter((line) => line.contains.includes(allergen))
+      .map((line) => line.values)
+      .reduce((acc, el) => {
+        return acc.filter((val) => el.includes(val));
+      }, Object.keys(ingredients));
+
+    match[allergen] = ingredientsForAllergen;
+  });
+
+  while (Object.values(allergens).some((el) => el === null)) {
+    const keys = Object.keys(match);
+    for (let i = 0; i < keys.length; i += 1) {
+      const key = keys[i];
+      if (match[key].length === 1) {
+        const element = match[key][0];
+        allergens[key] = element;
+        for (let j = 0; j < keys.length; j += 1) {
+          match[keys[j]] = match[keys[j]].filter((el) => el !== element);
+        }
+      }
+    }
+  }
+
+  return inputFormatted.reduce((acc, el) => {
+    acc += el.values.filter((ingredient) => !Object.values(allergens).includes(ingredient)).length;
+    return acc;
+  }, 0);
+}
+
+console.log(`Result of day21/easy -> ${main()}`);
